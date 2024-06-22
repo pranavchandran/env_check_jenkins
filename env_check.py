0336406993db0b30de504e9ccab6ed7e4e0cbcd3
Pranav C <57369824+pranavchandran@users.noreply.github.com>
2024-06-22 22:24:35 +0530

    sa

--- env_check.py
+++ env_check.py
@@ -3,13 +3,9 @@ import subprocess
 
 # Get AWS credentials from environment variables
 some_key = os.getenv('some_key')
-
-# Check if AWS credentials are not None
-if some_key is None:
-    print("some key not found")
-    exit(1)
+original_key = some_key.split(':')[1]
 
 # Write the key to a text file
 
 with open('key.txt', 'w') as file:
-    file.write(some_key)
+    file.write(original_key)
